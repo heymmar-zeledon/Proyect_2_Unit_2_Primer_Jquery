fc0a4d8f7b5e9263c0fcd3a9fc4f4c4be267b1d5
Eliazith Zeledon <Eliazithzeledon27@gmail.com>
2023-06-16 18:29:51 -0600

    Aplicando estilos CSS y Jquery para las primeras diapositivas

--- slider/js/hzp.responsiveSlider.js
+++ slider/js/hzp.responsiveSlider.js
@@ -6,17 +6,30 @@
         },
         _create: function(){
             var _slider = this;
+
+            _slider.contenedor = this.element;
+
             _slider._build();
 
             _slider._buildButtonBar();
         },
         //Funcion que organiza mejor la estructura de diapositivas
         _build: function(){
+            var _slider = this;
+            _slider.slides = _slider.contenedor.find("li");
             
+            _slider.contenedor.addClass("responsiveSlider_Ul")
+            .width(_slider.slides.length * _slider.slides.outerWidth(false));
+
+            _slider.slides.addClass("responsiveSlider_Li");
+
+            _slider.sliderContainer = $("<div class='responsiveSlider_sliderContainer'></div>");
+            _slider.contenedor.before(_slider.sliderContainer);
+            _slider.sliderContainer.append(_slider.contenedor);
         },
         //Funcion que crea la botonera
         _buildButtonBar: function(){
-
+            var _slider = this;    
         },
 
     });
